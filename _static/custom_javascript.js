16ff33401631db41e94fb9a9505c5fa50409debc
fdrgsp <federico.gasparoli@gmail.com>
2025-01-16 20:32:28 -0500

    custom_javascript

--- _static/custom_javascript.js
+++ _static/custom_javascript.js
@@ -5,21 +5,8 @@ document.addEventListener('DOMContentLoaded', function () {
         link.addEventListener('click', function (event) {
             const href = link.getAttribute('href');
             if (href) {
-                if (href.endsWith('.pdf')){
-                    event.preventDefault(); // Prevent navigation to the folder
-                    const downloadLink = document.createElement('a');
-                    downloadLink.href = href; // Set the href as the download URL
-                    downloadLink.download = href.split('/').pop(); // Use the file name for download
-                    downloadLink.click(); // Programmatically click the link to trigger download
-                }
-                if (href.endsWith('.tif' || '.tiff' || '.png' || '.jpg' || '.jpeg')){
-                    event.preventDefault(); // Prevent navigation to the folder
-                    const downloadLink = document.createElement('a');
-                    downloadLink.href = href; // Set the href as the download URL
-                    downloadLink.download = href.split('/').pop(); // Use the file name for download
-                    downloadLink.click(); // Programmatically click the link to trigger download
-                }
-                if (href.endsWith('.zip')){
+                const fileExtensions = ['.pdf', '.tif', '.tiff', '.png', '.jpg', '.jpeg', '.zip'];
+                if (fileExtensions.some(ext => href.endsWith(ext))) {
                     event.preventDefault(); // Prevent navigation to the folder
                     const downloadLink = document.createElement('a');
                     downloadLink.href = href; // Set the href as the download URL
